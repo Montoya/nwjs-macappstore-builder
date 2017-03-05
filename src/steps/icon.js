f764d2f735ca0363c9394ddd6693f7dd7e42137f
Christian Montoya <sirokai@gmail.com>
2017-03-04 23:35:28 -0500

    don't call the same callback twice!

--- src/steps/icon.js
+++ src/steps/icon.js
@@ -8,13 +8,9 @@ m.installIcon = function(app_path, icon_path, callback)
 {
     var rm_command = 'rm "' + app_path + '/Contents/Resources/app.icns"';
     var cp_command = 'cp "' + icon_path + '" "' + app_path + '/Contents/Resources/app.icns"';
-    exec(rm_command + ' && ' + cp_command, function(error)
-    {
-        callback(error);
-    });
     var rm_command_2 = 'rm "' + app_path + '/Contents/Resources/document.icns"';
     var cp_command_2 = 'cp "' + icon_path + '" "' + app_path + '/Contents/Resources/document.icns"';
-    exec(rm_command_2 + ' && ' + cp_command_2, function(error)
+    exec(rm_command + ' && ' + cp_command + ' && ' + rm_command_2 + ' && ' + cp_command_2, function(error)
     {
         callback(error);
     });
